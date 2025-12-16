9fbba9236e711f2fd0815d813416c857f1e07a6e
anhthi2714 <152952446+anhthi2714@users.noreply.github.com>
2025-12-16 15:50:48 +0700

    Add LCD 20x4

--- firmware/irrigation/include/config.h
+++ firmware/irrigation/include/config.h
@@ -15,6 +15,10 @@ const int MQTT_PORT = 1883;
 #define TRIG_PIN 26
 #define ECHO_PIN 25
 #define DHT_PIN 16 // Chân GPIO 16 được sử dụng để kết nối cảm biến DHT
+#define LCD_ADDR 0x27 // Cấu hình LCD I2C
+#define LCD_COLS 20     
+#define LCD_ROWS 4      
+#define MAX_DISTANCE_CM 50 // Ngưỡng khoảng cách (nếu có người đến gần 50cm, màn hình bật)
 // --- 3. MQTT Topics (Nên gom nhóm) ---
 // Thay 22120421 bằng ID của bạn
 #define TOPIC_SENSOR_DATA   "irrigation/sensors"   // Gửi data (Độ ẩm, nước, nhiệt độ...)
